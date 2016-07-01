06c4450e93270320e9b9c3ca104b20b81159c37f
Tijo <emailtijothomas@gmail.com>
2016-07-01 18:34:35 -0500

    Adds update action to MoviesController.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,4 +12,10 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(params[:id])
 	end
 
+	def update
+		@movie = Movie.find(params[:id])
+		movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross) # Returns a new hash that includes only the permitted attributes. The .require method will raise an exception if the given key ':movie' in this case isn't found in the params object.
+		@movie.update(movie_params)
+	end
+
 end
