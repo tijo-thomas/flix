9f866d8c8dfba5266ec47583f573a4933e4c8493
Tijo <emailtijothomas@gmail.com>
2016-07-01 18:15:43 -0500

    Adds patch route. Adds code example to edit_movie_spec to assert on update action.

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -4,7 +4,7 @@ describe "Editing a movie" do
 	
 	it "updates the movie and shows the movie's updated details" do
 		movie = Movie.create(movie_attributes)
-		
+
 		visit movie_url(movie.id)
 
 		click_link "Edit"
@@ -12,6 +12,14 @@ describe "Editing a movie" do
 		expect(current_path).to eq(edit_movie_path(movie.id))
 
 		expect(find_field("Title").value).to eq(movie.title)
+
+		fill_in "Title", with: "Updated Movie Title"
+
+		click_button "Update Movie"
+
+		expect(current_path).to eq(movie_path(movie.id))
+
+		expect(page).to have_text("Updated Movie Title")
 	end
 
 end
