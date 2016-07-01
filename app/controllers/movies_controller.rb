42f1943141007acd30155da7ff96f1b39527db78
Tijo <emailtijothomas@gmail.com>
2016-07-01 18:44:53 -0500

    Redirects the submitted form from the update action to the show action. Also cleans up the show page.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,6 +16,7 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(params[:id])
 		movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross) # Returns a new hash that includes only the permitted attributes. The .require method will raise an exception if the given key ':movie' in this case isn't found in the params object.
 		@movie.update(movie_params)
+		redirect_to movie_path(@movie.id) # This redirects to the show action using a named route.
 	end
 
 end
