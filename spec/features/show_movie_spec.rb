89a54c284003cd20b2fb777314791b6dc3f83a06
Tijo <emailtijothomas@gmail.com>
2016-07-02 15:31:51 -0500

    Updates spec with cast, director, and duration new fields.

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -12,6 +12,9 @@ describe "Viewing an individual movie" do
 		expect(page).to have_text(movie.rating)
 		expect(page).to have_text(movie.description)
 		expect(page).to have_text(movie.released_on)
+		expect(page).to have_text(movie.cast)
+		expect(page).to have_text(movie.director)
+		expect(page).to have_text(movie.duration)
 	end
 
 	it "shows the total gross if the total goss exceeds $50M" do
