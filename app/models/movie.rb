8bb0e41eb35716530db9293a85538439de261082
Tijo <emailtijothomas@gmail.com>
2016-07-02 12:05:08 -0500

    Adds .hits, .flops, .recently_added methods to query the database in the Movie model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,4 +7,16 @@ class Movie < ActiveRecord::Base
 	def self.released
 		where("released_on <= ?", Time.now).order("released_on desc")
 	end
+
+	def self.hits
+		where("total_gross >= 300000000").order("total_gross desc")
+	end
+
+	def self.flops
+		where("total_gross <= 50000000").order("total_gross asc")
+	end
+
+	def self.recently_added
+		order("created_at desc").limit(3)
+	end
 end
