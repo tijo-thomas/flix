f4c6dbaf5fd5e9c566551a24963e3609b1a9c5d2
Tijo <emailtijothomas@gmail.com>
2016-07-01 19:58:21 -0500

    Comments out existing routes and uses the resources convention to generate all routes for movies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
   root "movies#index"
+  resources :movies
 
-  get "movies" => "movies#index"
-  get "movies/new" => "movies#new"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
+  # get "movies" => "movies#index"
+  # get "movies/new" => "movies#new"
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # patch "movies/:id" => "movies#update"
 
 end
