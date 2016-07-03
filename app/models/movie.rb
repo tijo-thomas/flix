7829c389c6bd513831883ca80bfab7299c74fffb
Tijo <emailtijothomas@gmail.com>
2016-07-03 16:14:40 -0500

    Fixes spelling mistake in Movie model validation. Moves placement of RATINGS constant above validation that uses it.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,16 @@
 class Movie < ActiveRecord::Base
 
-	validates :title, :released_on, :duration, prescence: true
+	validates :title, :released_on, :duration, presence: true
 	validates :description, length: { minimum: 25 }
 	validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
 	validates :image_file_name, allow_blank: true, format: {
 		with: 		/\w+\.(gif|jpg|png)\z/i,
 		message:  "must reference a GIF, JPG, or PNG image"
 	}
-	validates :rating, inclusion: { within: RATINGS }
-
 	# Array constant that contains all possibile ratings.
 	RATINGS = %w(G PG PG-13 R NC-17) 
+	validates :rating, inclusion: { within: RATINGS }
+
 
 	def flop?
 		total_gross.blank? || total_gross < 50000000
