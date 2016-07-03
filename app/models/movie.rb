b41382c28fd55e5be93189f932d7bebf54365836
Tijo <emailtijothomas@gmail.com>
2016-07-03 16:03:16 -0500

    Adds validations for Movie model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,17 @@
 class Movie < ActiveRecord::Base
 
+	validates :title, :released_on, :duration, prescence: true
+	validates :description, length: { minimum: 25 }
+	validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+	validates :image_file_name, allow_blank: true, format: {
+		with: 		/\w+\.(gif|jpg|png)\z/i,
+		message:  "must reference a GIF, JPG, or PNG image"
+	}
+	validates :rating, inclusion: { within: RATINGS }
+
+	# Array constant that contains all possibile ratings.
+	RATINGS = %w(G PG PG-13 R NC-17) 
+
 	def flop?
 		total_gross.blank? || total_gross < 50000000
 	end
