718c7872ce83753accdfb0355b23cfc5875d1b57
Tijo <emailtijothomas@gmail.com>
2016-07-04 00:35:34 -0500

    Adds new specs.

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -7,19 +7,31 @@ describe "Viewing the list of movies" do # this is an example group that groups 
 													rating: "PG-13",
 													total_gross: 318412101.00,
 													description: "Tony Stark builds an armored suit to fight the throes of evil.",
-													released_on: "2008-05-02")
+													released_on: "2008-05-02",
+													cast: "Robert Downey Jr., Gwyneth Paltrow, and Terrence Howard",
+													director: "Jon Favreau",
+													duration: "126 min",
+													image_file_name: "ironman.jpg")
 
 		movie2 = Movie.create(title: "Superman",
 													rating: "PG",
 													total_gross: 134218018.00,
 													description: "Clark Kent grows up to be the greatest super-hero.",
-													released_on: "1978-12-15")
+													released_on: "1978-12-15",
+													cast: "Christopher Reeve, Margot Kidder, and Gene Hackman",
+													director: "Richard Donner",
+													duration: "143 min",
+													image_file_name: "superman.jpg")
 
 		movie3 = Movie.create(title: "Spider-Man",
 													rating: "PG-13",
 													total_gross: 403706375.00,
 													description: "Peter Parker gets bit by a genetically modified spider.",
-													released_on: "2002-05-03")
+													released_on: "2002-05-03",
+													cast: "Tobey Maguire, Kirsten Dunst, and Willem Dafoe",
+													director: "Sam Raimi",
+													duration: "121 min",
+													image_file_name: "spiderman.jpg")
 
 		visit movies_url
 
@@ -32,6 +44,8 @@ describe "Viewing the list of movies" do # this is an example group that groups 
 		expect(page).to have_text(movie1.description[0..9])
 		expect(page).to have_text(movie1.released_on)
 		expect(page).to have_text("$318,412,101.00")
+		expect(page).to have_text(movie1.cast)
+		expect(page).to have_text(movie1.duration)
 	end
 
 	it "does not show a movie that hasn't yet been released" do
