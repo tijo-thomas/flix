0ade2028add367c22072d53039b4e4f04ae48c9f
Tijo <emailtijothomas@gmail.com>
2016-07-05 15:54:17 -0500

    Fixes spelling issue with comments: validation of Review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :movie
 
   validates :name, presence: true
-  validates :comments, length: { minimum: 4 }
+  validates :comment, length: { minimum: 4 }
   STARS = [1, 2, 3, 4, 5]
   validates :stars, inclusion: {
   	within: STARS,
