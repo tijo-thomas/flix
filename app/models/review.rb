29339645146b8cb279443bc595b90a98db7d726e
Tijo <emailtijothomas@gmail.com>
2016-07-05 15:47:56 -0500

    Adds validations to Review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
+
+  validates :name, presence: true
+  validates :comments, length: { minimum: 4 }
+  STARS = [1, 2, 3, 4, 5]
+  validates :stars, inclusion: {
+  	within: STARS,
+  	message: "must be between 1 and 5"
+  }
 end
