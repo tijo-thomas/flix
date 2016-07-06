163a1415ff15f0dbf22a0117c9958259e78a6bd2
Tijo <emailtijothomas@gmail.com>
2016-07-06 00:14:54 -0500

    Adds new action of ReviewController.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,9 @@ class ReviewsController < ApplicationController
 		@movie = Movie.find(params[:movie_id])
 		@reviews = @movie.reviews
 	end
+
+	def new
+		@movie = Movie.find(params[:movie_id])
+		@review = @movie.reviews.new
+	end
 end
