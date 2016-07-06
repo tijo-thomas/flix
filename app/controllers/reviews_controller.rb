758dfcbb14f9cc4d0bf2264f4069ef05c29de7e3
Tijo <emailtijothomas@gmail.com>
2016-07-06 12:09:52 -0500

    Adds the create action as well as the private section that includes the review params. Also, fixes mistakes in the review new template.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,4 +9,23 @@ class ReviewsController < ApplicationController
 		@movie = Movie.find(params[:movie_id])
 		@review = @movie.reviews.new
 	end
+
+	def create
+		@movie = Movie.find(params[:movie_id])
+		@review = @movie.reviews.new(review_params)
+		if @review.save
+			redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	# Returns a new hash that includes only the permitted attributes. The .require method will raise an exception if the given key ':review' in this case isn't found in the params object.
+	def review_params
+		params.require(:review).permit(:name, :comment, :stars)
+	end
+
+
 end
