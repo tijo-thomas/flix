432813833c97ed36402e51e540a6f1cb73f85683
Tijo <emailtijothomas@gmail.com>
2016-07-06 19:52:14 -0500

    Adds 'average_stars' method to Movie class. Displays 'average_stars' in the show template of movie using the conditional view logic method(format_average_stars) found in the movies_helper.

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -18,4 +18,12 @@ module MoviesHelper
 		end
 	end
 
+	def format_average_stars(movie)
+		if movie.average_stars.nil?
+			content_tag(:strong, "No Reviews")
+		else
+			pluralize(number_with_precision(movie.average_stars, precision: 1), "star")
+		end
+	end
+
 end
