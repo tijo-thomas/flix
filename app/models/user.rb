1eaa31ee7cc485f7782da0a200b7d711f248f744
Tijo <emailtijothomas@gmail.com>
2016-07-08 20:13:30 -0500

    Adds validation for the password virtual attribute of User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
   									format: { with: /\A\S+@\S+\z/ },
   									uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 10, allow_blank: true } # The 'allow_blank: true' is important since a password isn't required when a user updates his name and/or email. Also, the :password is a virtual attribute since the database column is password_digest.
 end
