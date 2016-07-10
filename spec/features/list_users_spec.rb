160d90145542080921bf44ee6ded9d11b994b404
Tijo <emailtijothomas@gmail.com>
2016-07-10 16:50:07 -0500

    Adds missing 'end' keyword to file.

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -13,9 +13,4 @@ describe "Viewing the list of users" do
     expect(page).to have_link(user2.name)
     expect(page).to have_link(user3.name)
   end
-
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 70ea45cc1287985c9ad18fd0f3ddd390b5d32640
