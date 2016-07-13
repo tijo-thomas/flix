46dde4c36b0e53f16ee3a159bb6b258833363dab
Tijo <emailtijothomas@gmail.com>
2016-07-13 17:47:06 -0500

    Adds code examples to test authentication.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -13,4 +13,38 @@ describe "Signing in" do
     expect(page).to have_field("Password")
   end
 
+  # in spec/features/sign_in_spec.rb
+
+  it "signs in the user if the email/password combination is valid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button 'Sign In'
+
+    expect(current_path).to eq(user_path(user))   
+
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it "does not sign in the user if the email/password combination is invalid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "no match"
+
+    click_button 'Sign In'
+
+    expect(page).to have_text('Invalid')
+  end
+
 end
