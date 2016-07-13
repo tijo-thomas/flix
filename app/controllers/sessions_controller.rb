3b3c9184f97f31decfca5f1bb69a0b2c3f148d45
Tijo <emailtijothomas@gmail.com>
2016-07-13 18:47:16 -0500

    Implements the create action of the Sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,29 @@ class SessionsController < ApplicationController
 		
 	end
 	
+	# Finds the user that matches the submitted email
+	# and assigns to variable 'user'. Then verifies
+	# that the submitted password is correct for that
+	# user.
+	# The .find_by can return nil so we have to use a
+	# conditional to check whether the user was authenticated
+	# or not.
 	def create
-		
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			flash[:notice] = "Welcome back, #{user.name}!"
+			redirect_to user_path(user.id)
+		else
+			# We use 'flash.now' because it is not coming before
+			# a redirect request. Flashes by default get picked
+			# up in the next request, but here we are just
+			# re-rendering the form so we use 'flash.now' to
+			# make the flash message available in the same request
+			# instead of wainting for the next request.
+			flash.now[:alert] = "Invalid email/password combination!"
+			render :new
+		end
 	end
 
 end
