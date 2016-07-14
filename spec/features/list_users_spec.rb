3a9533bb9982c700985c8afec100502c0bde8d11
Tijo <emailtijothomas@gmail.com>
2016-07-13 19:43:43 -0500

    Adds username to the arrange portion of the code example.

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe "Viewing the list of users" do
 
   it "shows the users" do
-    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com"))
-    user2 = User.create!(user_attributes(name: "Moe",   email: "moe@example.com"))
-    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com"))
+    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com", username: "larryflix"))
+    user2 = User.create!(user_attributes(name: "Moe",   email: "moe@example.com", username: "moeflix"))
+    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com", username: "curlyflix"))
 
     visit users_url
 
