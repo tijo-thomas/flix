2c42204cba95903742f93da5e5b1e107a136ccd6
Tijo <emailtijothomas@gmail.com>
2016-07-14 04:41:35 -0500

    Optimizes the 'current_user' method to only run query when needed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,12 @@ module ApplicationHelper
 	# user id before calling find. This is in the
 	# application_helper.rb because it should be able to be
 	# called throughout the application.
+	# Uses the ||= operator for memoization. This optimizes
+	# the current_user method by storing the result of calling
+	# User.find in an instance variable and only running the
+	# query again if that instance variable doesn't already
+	# have a value.
 	def current_user
-		User.find(session[:user_id]) if session[:user_id]
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 end
