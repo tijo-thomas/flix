645ada4bd21778056bca7e9b527e14c4281a35e0
Tijo <emailtijothomas@gmail.com>
2016-07-14 03:45:09 -0500

    Refactors code into a custom view helper method, 'current_user', that returns the currently signed in user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	# The find method will raise an exception if passed value
+	# of nil. So we check that the session actually has a
+	# user id before calling find. This is in the
+	# application_helper.rb because it should be able to be
+	# called throughout the application.
+	def current_user
+		User.find(session[:user_id]) if session[:user_id]
+	end
 end
