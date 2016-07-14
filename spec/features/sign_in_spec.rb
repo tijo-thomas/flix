4532f8297028643394b11274973e39f1f798f495
Tijo <emailtijothomas@gmail.com>
2016-07-14 03:06:58 -0500

    Adds code examples to the sign_in_spec to remove sign in/out links when signed in and vice versa.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -30,6 +30,9 @@ describe "Signing in" do
     expect(current_path).to eq(user_path(user))   
 
     expect(page).to have_text("Welcome back, #{user.name}!")
+    expect(page).to have_link(user.name)
+    expect(page).not_to have_link("Sign In")
+    expect(page).not_to have_link("Sign Up")
   end
 
   it "does not sign in the user if the email/password combination is invalid" do
@@ -45,6 +48,9 @@ describe "Signing in" do
     click_button 'Sign In'
 
     expect(page).to have_text('Invalid')
+    expect(page).not_to have_link(user.name)
+    expect(page).to have_link("Sign In")
+    expect(page).to have_link("Sign Up")
   end
 
 end
