299b859a6a9d56becb69a410985a05fca9db30ac
Tijo <emailtijothomas@gmail.com>
2016-07-14 04:16:49 -0500

    Adds code example to test automatically signing in after creating account.

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -32,4 +32,11 @@ describe "Creating a new user" do
 
     expect(page).to have_text('error')
   end
+
+  it "saves the user and shows the user's profile page" do    
+    # existing code
+
+    expect(page).not_to have_link('Sign In')
+    expect(page).not_to have_link('Sign Up')
+  end
 end
