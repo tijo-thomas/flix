dc9268de1072c099a3eedc1c4b28667d7eed73ca
Tijo <emailtijothomas@gmail.com>
2016-07-14 20:05:43 -0500

    Adds user argument to sign_in method that was forgotten at first.

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,5 +1,5 @@
 # This method walks through the sign in process as a test.
-def sign_in
+def sign_in(user)
 	visit new_session_url
 	fill_in "Email", with: user.email
 	fill_in "Password", with: user.password
