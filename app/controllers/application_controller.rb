614f60d6f148c70fd946fe611784603cb70fb940
Tijo <emailtijothomas@gmail.com>
2016-07-18 18:14:15 -0500

    Moves the current_user method from application_helper.rb to the Application controller so it can be called by the require_signin method in the Application controller. This is because helper methods can't be called from inside controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,18 @@ class ApplicationController < ActionController::Base
   		redirect_to new_session_url, alert: "Please sign in first!"
   	end
   end
+
+  # The find method will raise an exception if passed value
+  # of nil. So we check that the session actually has a
+  # user id before calling find. This is in the
+  # application_helper.rb because it should be able to be
+  # called throughout the application.
+  # Uses the ||= operator for memoization. This optimizes
+  # the current_user method by storing the result of calling
+  # User.find in an instance variable and only running the
+  # query again if that instance variable doesn't already
+  # have a value.
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
