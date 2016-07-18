2b71d0a0cae57c10589dff47910b170cee3318d1
Tijo <emailtijothomas@gmail.com>
2016-07-18 18:05:09 -0500

    Adds require_signin method to Application controller. This method is called in the before_action of the Users controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   add_flash_types(:danger) # This is how to register custom flash types while redirecting.
+
+
+  private
+
+  # Unless a user is signed in, we redirect to the new_session_url, including and alert message.
+  # Set as private so it's not exposed to users as a
+  # web accessible action.
+  def require_signin
+  	unless current_user
+  		redirect_to new_session_url, alert: "Please sign in first!"
+  	end
+  end
 end
