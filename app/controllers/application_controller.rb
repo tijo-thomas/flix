2b3dbce4df7e39be23d3a326b8b5e02c70639ced
Tijo <emailtijothomas@gmail.com>
2016-07-25 18:08:57 -0500

    Adds current_user_admin? method that checks to see if there is a current user and that user is an admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,8 +43,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user?
 
   def require_admin?
-    unless current_user.admin?
+    unless current_user_admin?
       redirect_to root_url, alert: "Unauthorized access!"
     end
+  end
+
+  # Returns true if there's a current user and that user
+  # is an admin.
+  def current_user_admin?
+    current_user && current_user.admin?
   end
 end
