029294187bbc75ab45812f0adcade2371c6d6ba8
Tijo <emailtijothomas@gmail.com>
2016-07-25 17:16:26 -0500

    Adds admin field to Users table and migrates.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712225224) do
+ActiveRecord::Schema.define(version: 20160725215038) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -42,9 +42,10 @@ ActiveRecord::Schema.define(version: 20160712225224) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "username"
+    t.boolean  "admin",           default: false
   end
 
 end
