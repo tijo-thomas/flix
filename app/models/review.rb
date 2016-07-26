347307508646a2c013ca1dd591f63ef14eabe86a
Tijo <emailtijothomas@gmail.com>
2016-07-26 18:24:41 -0500

    Removes the name attribute validation since it's no longer a review attribute.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,6 @@ class Review < ActiveRecord::Base
   belongs_to :movie
   belongs_to :user
 
-  validates :name, presence: true
   validates :comment, length: { minimum: 4 }
   STARS = [1, 2, 3, 4, 5]
   validates :stars, inclusion: {
