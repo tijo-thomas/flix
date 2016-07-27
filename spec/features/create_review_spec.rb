df425003a455e42544303e7429d4cf6c13f29ef4
Tijo <emailtijothomas@gmail.com>
2016-07-27 18:10:03 -0500

    Updates spec to reflect that authorization is now required before creating a review.

--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe "Creating a new review" do
+
+  before do
+    @user = User.create!(user_attributes)
+    sign_in(@user)
+  end
+  
   it "saves the review" do
     movie = Movie.create(movie_attributes)
 
