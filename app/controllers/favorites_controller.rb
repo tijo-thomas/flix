a4fff2ea7d4fe0093e63ea3d549060947ba913ad
Tijo <emailtijothomas@gmail.com>
2016-07-29 18:36:13 -0500

    Implements the create action in Favorites controller.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,9 @@
 class FavoritesController < ApplicationController
+	before_action :require_signin
+
+	def create
+		@movie = Movie.find(params[:movie_id])
+		@movie.favorites.create(user: current_user)
+		redirect_to movie_path(@movie.id), notice: "Thanks for fav'ing!"
+	end
 end
