f9ca8117e23af07820121860977e35f93de08478
Tijo <emailtijothomas@gmail.com>
2016-07-29 18:08:30 -0500

    Nests the favorites resource inside the movies resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :favorites
   get "signin" => "sessions#new"
   resource :session  
   get "signup" => "users#new"
   resources :users
   root "movies#index"
   resources :movies do
-  	resources :reviews
+    resources :reviews
+    resources :favorites
   end
 
   # get "movies" => "movies#index"
