a299b25f60bcbea8eede285afae3645594c7e741
Tijo <emailtijothomas@gmail.com>
2016-07-30 16:45:38 -0500

    Adds a helper method to display the content_for. This is because we only want to see the hyphen in the page title only when content_for has a value.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
 
+	# This helper method displays the hyphen in the
+	# content_for helper method if there is a value
+	# returned from content_for, otherwise the content_for
+	# just displays "Flix" as the page title.
+	def page_title
+		if content_for?
+			"Flix - #{content_for(:title)}"
+		else
+			"Flix"
+		end
+	end
 
 end
