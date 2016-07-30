54831e4f4efab3a2f3706edac4712dc9a888a53e
Tijo <emailtijothomas@gmail.com>
2016-07-30 14:00:53 -0500

    Sets up a movie's genres by calling @movies.genres in the show action.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,7 @@ class MoviesController < ApplicationController
 	def show
 		@movie = Movie.find(params[:id])
 		@fans = @movie.fans
+		@genres = @movie.genre
 
 		if current_user
 			@current_favorite = current_user.favorites.find_by(movie_id: @movie.id)
