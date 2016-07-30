10f668e04c44cd5ef91b55be0fd6bef1601d56a5
Tijo <emailtijothomas@gmail.com>
2016-07-30 14:18:24 -0500

    Fixes spelling mistake in the show action.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
 	def show
 		@movie = Movie.find(params[:id])
 		@fans = @movie.fans
-		@genres = @movie.genre
+		@genres = @movie.genres
 
 		if current_user
 			@current_favorite = current_user.favorites.find_by(movie_id: @movie.id)
