72fbb5356cffabb31619111f46d2b4096ae88863
Tijo <emailtijothomas@gmail.com>
2016-07-30 14:37:09 -0500

    Removes '?' from require_admin method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user?
 
-  def require_admin?
+  def require_admin
     unless current_user_admin?
       redirect_to root_url, alert: "Unauthorized access!"
     end
