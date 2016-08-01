5da2f36c880152ba1e33d584bab83c2dd7545044
Tijo <emailtijothomas@gmail.com>
2016-08-01 16:10:29 -0500

    Converts the self.hits and self.flops methods into scopes(custom queries that are callable).

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -19,6 +19,8 @@ class Movie < ActiveRecord::Base
 	has_many :genres, through: :characterizations
 
 	scope :released, -> { where("released_on <= ?", Time.now).order(released_on: :desc) }
+	scope :hits, -> { where("total_gross >= 300000000").order(total_gross: :desc) }
+	scope :flops, -> { where("total_gross <= 50000000").order(total_gross: :asc) }
 
 	def flop?
 		total_gross.blank? || total_gross < 50000000
@@ -28,13 +30,13 @@ class Movie < ActiveRecord::Base
 	# 	where("released_on <= ?", Time.now).order("released_on desc")
 	# end
 
-	def self.hits
-		where("total_gross >= 300000000").order("total_gross desc")
-	end
+	# def self.hits
+	# 	where("total_gross >= 300000000").order("total_gross desc")
+	# end
 
-	def self.flops
-		where("total_gross <= 50000000").order("total_gross asc")
-	end
+	# def self.flops
+	# 	where("total_gross <= 50000000").order("total_gross asc")
+	# end
 
 	def self.recently_added
 		order("created_at desc").limit(3)
